8d5eb7bcbece61dd91f925e77817a7b49e9f7ae8
Sasha <sashainkiev@gmail.com>
2011-11-11 16:11:27 +0200

    clean start point

---
+++ README.md
@@ -0,0 +1,4 @@
+The project for learning OAuth and MongoDB
+==========================================
+
+[Tutorial](https://github.com/RailsApps/rails3-mongoid-omniauth/wiki/Tutorial)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oauthmongo::Application.config.secret_token = 'a6af6e957ea4957bd4c86f275f3e651b6bd17387922201eb4770c697bd6767490031d009dcbd89ab15ca2cd94b3ddc53144b37c8fa67676d67232b07914ec66d'
